e00420aa2a9b7ba9a7837a6b0ad4b71fe96cc9c5
v.karmazin <v.karmazin@tinkoff.ru>
2020-07-25 20:23:49 +0300

    UPD: Tests classes to functions

--- tests/sed/test_misc.py
+++ tests/sed/test_misc.py
@@ -1,56 +1,60 @@
+# pylint: disable=protected-access
+
 import pytest
 
 from coreutils import sed
 from coreutils.sed import SedFlags
 
 
-class TestSedMisc:
-    @pytest.mark.parametrize(
-        'command, expected',
-        [
-            ('probably', True),
-            ('perdak', False,),
-            (r'\w+ s c.*d', True),
-            (r'\d.*', False),
-            (lambda l: True, True,),
-            (lambda l: False, False,),
-            (lambda l: 'substitute' in l, True,),
-            (lambda l: 'substitute' not in l, False,),
-        ],
-    )
-    def test_match_command(self, command, expected):
-        string = 'The s command (as in substitute) is probably'
-        result = sed.match_string(string, command)
-        assert result == expected
+@pytest.mark.skip(reason='TODO: move to search')
+@pytest.mark.parametrize(
+    'command, expected',
+    [
+        ('probably', True),
+        ('perdak', False),
+        (r'\w+ s c.*d', True),
+        (r'\d.*', False),
+        (lambda text: True, True),
+        (lambda text: False, False),
+        (lambda text: 'substitute' in text, True),
+        (lambda text: 'substitute' not in text, False),
+    ],
+)
+def test_match_command(command, expected):
+    string = 'The s command (as in substitute) is probably'
+    result = sed._match_line(string, command, flags=frozenset())
+    assert result == expected
 
-    @pytest.mark.skip(reason='no way of currently testing this')
-    @pytest.mark.parametrize(
-        'flag, expected',
-        [
-            (None, True),
-            (SedFlags.GLOBAL, False),
-            (SedFlags.PRINT, False),
-            (SedFlags.EXECUTE, False),
-            (SedFlags.INSENSITIVE, False),
-            (SedFlags.DELETE, False),
-        ],
-    )
-    def test_match_with_flag(self, flag, expected):
-        string = 'The s command (as in substitute) is probably'
-        command = 'The s c.*d'
-        result = sed.match_string(string, command, flags={flag})
-        assert result == expected
 
-    @pytest.mark.skip(reason='no way of currently testing this')
-    @pytest.mark.parametrize(
-        'flags, expected',
-        [
-            (frozenset({SedFlags.GLOBAL, SedFlags.PRINT}), False),
-            (frozenset({SedFlags.DELETE, SedFlags.EXECUTE, SedFlags.INSENSITIVE}), False),
-        ],
-    )
-    def test_match_with_several_flags(self, flags, expected):
-        string = 'The s command (as in substitute) is probably'
-        command = 'The s c.*d'
-        result = sed.match_string(string, command, flags=flags)
-        assert result == expected
+@pytest.mark.skip(reason='no way of currently testing this')
+@pytest.mark.parametrize(
+    'flag, expected',
+    [
+        (None, True),
+        (SedFlags.GLOBAL, False),
+        (SedFlags.PRINT, False),
+        (SedFlags.EXECUTE, False),
+        (SedFlags.INSENSITIVE, False),
+        (SedFlags.DELETE, False),
+    ],
+)
+def test_match_with_flag(flag, expected):
+    string = 'The s command (as in substitute) is probably'
+    command = 'The s c.*d'
+    result = sed._match_line(string, command, flags={flag})
+    assert result == expected
+
+
+@pytest.mark.skip(reason='no way of currently testing this')
+@pytest.mark.parametrize(
+    'flags, expected',
+    [
+        (frozenset({SedFlags.GLOBAL, SedFlags.PRINT}), False),
+        (frozenset({SedFlags.DELETE, SedFlags.EXECUTE, SedFlags.INSENSITIVE}), False),
+    ],
+)
+def test_match_with_several_flags(flags, expected):
+    string = 'The s command (as in substitute) is probably'
+    command = 'The s c.*d'
+    result = sed._match_line(string, command, flags=flags)
+    assert result == expected
