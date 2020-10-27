dcc04054d947a9e4840c039d9f65a2eee4b4ea80
readysloth <readysloth@protonmail.com>
2020-10-27 12:13:15 +0300

    feat(sed/__init__.py): добавил экспортируемые функции

--- coreutils/sed/__init__.py
+++ coreutils/sed/__init__.py
@@ -1,2 +1,2 @@
 from .utils import SedException, SedFlags
-from .sed import search, substitute, _is_processors_matched, _match_line
+from .sed import sed_search, sed_substitute, search, substitute, _is_processors_matched, _match_line
